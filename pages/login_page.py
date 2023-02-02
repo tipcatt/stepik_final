75f3a481893eb433072b886826d211fbdfc335b1
kturiatko <kostiantyn.turiatko@jiji.ng>
2023-02-02 23:51:49 +0200

    refactor + added checks for login page

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "It's not login page"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Missed Login form"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Missed Register form"
--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,14 @@
-from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_check_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_page()
