30db71df52aa410bb148e5601762adde64cb2dd2
kturiatko <kostiantyn.turiatko@jiji.ng>
2023-02-04 21:11:34 +0200

    added redirect between pages

--- test_main_page.py
+++ test_main_page.py
@@ -2,13 +2,15 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
-
-def test_check_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    login_page = LoginPage(browser, link)
-    login_page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+# def test_check_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+#     login_page = LoginPage(browser, link)
+#     login_page.open()
+#     login_page.should_be_login_page()
